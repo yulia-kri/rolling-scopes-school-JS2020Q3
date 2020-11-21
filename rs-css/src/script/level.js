8caf5f328ea394188b37284837ae851503ddee1f
Yuliya Kryvorot <krviua@yahoo.com>
2020-11-21 18:13:24 +0300

    feat: add animation

--- rs-css/src/script/level.js
+++ rs-css/src/script/level.js
@@ -4,12 +4,16 @@ import { nextLevel } from './index';
 const displayTask = document.querySelector('.task');
 const displayGame = document.querySelector('.game-wrapper');
 const displayHTML = document.querySelector('.html__markup');
+const spinAnimation = 'spin 8s infinite linear';
 
 export function displayLevel(level) {
   const content = levels[level];
   displayTask.innerText = content.task;
   displayGame.innerHTML = content.html;
   displayHTML.innerHTML = convertHTML(content.html);
+
+  const DOMelements = selectAll(content.answer);
+  addAnimation(DOMelements, spinAnimation);
 }
 
 function convertHTML(str) {
@@ -35,14 +39,13 @@ export function submit(e, level) {
 }
 
 function levelCompleted(level, selector) {
+  const animation = 'slide-up 0.5s ease-in-out forwards';
   const checkmark = select(`[data-level='${level}']`).querySelector(
     '.level__checkmark'
   );
 
   const DOMelements = selectAll(selector);
-  DOMelements.forEach(
-    (elem) => (elem.style.animation = 'slide-up 0.5s ease-in-out forwards')
-  );
+  addAnimation(DOMelements, animation);
 
   checkmark.classList.add('completed');
 
@@ -55,4 +58,8 @@ function wrongAnswer() {
   setTimeout(() => {
     editor.style.animation = '';
   }, 700);
+}
+
+function addAnimation(elements, animation) {
+  elements.forEach((elem) => (elem.style.animation = animation));
 }
