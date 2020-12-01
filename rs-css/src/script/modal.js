d37ddf35b15f2c1d36251a0f3178b03b16ff7a75
Yuliya Kryvorot <krviua@yahoo.com>
2020-12-01 21:31:04 +0300

    feat: implement modal window

---
+++ rs-css/src/script/modal.js
@@ -0,0 +1,21 @@
+import { setCurrentLevel, getCurrentLevel } from './localStorage';
+import { displayLevel } from './level';
+
+export function showModal() {
+  const modalBg = document.querySelector('.modal-bg');
+  const modalBtns = modalBg.querySelector('.modal__buttons');
+
+  modalBg.classList.add('modal-bg--active');
+
+  modalBtns.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('modal__button')) return;
+    if (e.target.classList.contains('confirm')) {
+      const checkmarks = document.querySelectorAll('.level__checkmark');
+      localStorage.removeItem('progress');
+      checkmarks.forEach((elem) => (elem.className = 'level__checkmark'));
+      setCurrentLevel(0);
+      displayLevel(getCurrentLevel());
+    }
+    modalBg.classList.remove('modal-bg--active');
+  });
+}
